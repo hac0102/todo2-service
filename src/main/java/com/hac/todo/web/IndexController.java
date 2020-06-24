399e27a5ce8efd00b9fb4bb26b27b2c5098a1b8d
haChangJin <hac198912@gmail.com>
2020-06-24 13:51:11 +0900

    - login, logout   세션 없을시 login 페이지로   로그아웃시 login 페이지로

--- src/main/java/com/hac/todo/web/IndexController.java
+++ src/main/java/com/hac/todo/web/IndexController.java
@@ -1,6 +1,9 @@
 package com.hac.todo.web;
 
+import com.hac.todo.config.auth.LoginUser;
+import com.hac.todo.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,18 +12,27 @@ import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class IndexController {
 
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public ModelAndView login(ModelAndView mv) throws Exception{
-        mv.setViewName("login");
+    public ModelAndView login(ModelAndView mv, @LoginUser SessionUser user ) throws Exception{
+
+        if(user == null){
+            mv.setViewName("login");
+            return mv;
+        }
+
+        mv.setViewName("main");
         return mv;
     }
 
     @GetMapping("/logout2")
-    public ModelAndView logout(ModelAndView mv){
+    public ModelAndView logout(ModelAndView mv, @LoginUser SessionUser user){
+        log.info("로그아웃 name :: {}", user == null ? "null" : user.getName());
+        log.info("로그아웃 email :: {}", user == null ? "null" : user.getEmail());
         mv.setViewName("login");
         return mv;
     }
